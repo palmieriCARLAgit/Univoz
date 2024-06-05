ba4ee8fdaa0415b97aa827a0b99ce15c5caad88e
palmieriCARLAgit <carla.palmieri@genstudents.org>
2024-06-05 14:53:29 -0400

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRoutes, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRoutes>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRoutes>
+  );
+}
+
+export default App;
