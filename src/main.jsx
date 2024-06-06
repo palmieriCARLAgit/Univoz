6d08506f33f934d3f59660f2384a8e56f8b423c6
palmieriCARLAgit <carla.palmieri@genstudents.org>
2024-06-06 14:11:46 -0400

    qs

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Card1 from './Components/AboutComponents/AboutCard1.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Card1/>
   </React.StrictMode>,
 )
