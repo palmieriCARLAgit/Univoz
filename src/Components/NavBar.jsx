81e0bd61ed804a5df1f6ed3fff1fd84e21c3f9da
palmieriCARLAgit <carla.palmieri@genstudents.org>
2024-06-10 09:50:46 -0400

    cambios

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -37,17 +37,14 @@ function Navbar() {
             <div className=" col navbar position">
             <button  id="abrir-menu" onClick={ClickOpen}><i className="bi bi-list"></i></button> {/*  abrir menu---------->este es el estado inicial*/}
             <a href="#" className=" col-2 logo">UNIVOZ</a>
-            {/* <i  id='bi' class="bi bi-plus-lg"></i> */}
             <input id='searchBurguer' type="text" className="search-bar" placeholder=""/>
             <i id="lupaIcon" class="fa-solid fa-magnifying-glass"></i>
             <a id='create-post' href="/Login" className="create-post btn">Iniciar sesión</a> {/* Navigation item (Login) */}
                 <nav id="navbarBurguer" className={`col-10 nav  ${isVisible ? 'visible '  :'' }`}>   
-                 {/* <input type="text" className="search-bar" placeholder="Buscar en Univoz..."/>  */}
                     <button id="cerrar-menu" onClick={ClickClose}><i class="fa-solid fa-x"></i></button>     {/*  cerrar menu*/}
                     <ul id="BurguerLinks" className="nav-list ">
-                <li   className="list-group-item hoverActiveCursor BurguerLinks"><a href="#create-post"><i className="fas fa-house"></i> Inicio</a></li>
-                        <li   className="list-group-item hoverActiveCursor BurguerLinks "><a href="/normativas"><i className="fas fa-circle-info"></i> Información</a>
-                        </li>
+                       <li   className="list-group-item hoverActiveCursor BurguerLinks"><a href="#create-post"><i className="fas fa-house"></i> Inicio</a></li>
+                        <li   className="list-group-item hoverActiveCursor BurguerLinks "><a href="/normativas"><i className="fas fa-circle-info"></i> Información</a> </li>
                         <li  className="list-group-item hoverActiveCursor BurguerLinks"><a href="/quienes-somos"><i className="fas fa-person"></i> Quiénes Somos</a></li>
                         <li   className="list-group-item hoverActiveCursor BurguerLinks"><a href="/Login"><i class="fa-solid fa-right-to-bracket"></i>Iniciar sesión</a></li>
                     </ul>
