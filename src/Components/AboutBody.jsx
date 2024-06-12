bf6129a21dc43320935d0cbc867e9f52f69f8ed1
palmieriCARLAgit <carla.palmieri@genstudents.org>
2024-06-12 14:34:09 -0400

    commit final

--- src/Components/AboutBody.jsx
+++ src/Components/AboutBody.jsx
@@ -1,19 +1,7 @@
 import "./About.css";
 
 function AboutBody() {
-  // {
-  //   const nav= document.querySelector("#nav");
-  //   const abrir= document.querySelector("#abrir")
-  //   const cerrar= document.querySelector("#cerrar");
-  //   abrir.addEventListener("click", ()=>{
-  //     nav.classList.add("visible")
-      
-  //   })
-  //  cerrar.addEventListener("click", ()=>{
-  //     nav.classList.remove("visible")
-  // };
-
-  
+ 
 
   return (
     
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -18,15 +18,21 @@ function Navbar() {
 
     
     const [isVisible, setIsVisible] = useState(false);
-  
+//   funciones para abrir y cerrar el menu hambuerguesa
     const ClickOpen = () => {
       setIsVisible(true);
     };
     
-  
     const ClickClose = () => {
       setIsVisible(false);
     };
+ 
+    // intento de funcion para cambiar el bg de los href hamburguesa
+    function hoverActive(){
+        const select=document.querySelector("li")
+        select.style.backgroundColor = '#d6c1e7';
+        body.style.borderLeft = '4px solid #6a1b9a';
+    }
     
 
        
@@ -36,18 +42,23 @@ function Navbar() {
         <header id="headerIdNavbar" >
             <div className=" col navbar position">
             <button  id="abrir-menu" onClick={ClickOpen}><i className="bi bi-list"></i></button> {/*  abrir menu---------->este es el estado inicial*/}
-            <a href="#" className=" col-2 logo">UNIVOZ</a>
-            <input id='searchBurguer' type="text" className="search-bar" placeholder=""/>
-            <i id="lupaIcon" class="fa-solid fa-magnifying-glass"></i>
+            <a href="#" className=" col-2 logo logoext">UNIVOZ</a>
+            <input id='searchBurguer' type="text" className="search-bar" placeholder="Buscar.."/>
             <a id='create-post' href="/Login" className="create-post btn">Iniciar sesión</a> {/* Navigation item (Login) */}
-                <nav id="navbarBurguer" className={`col-10 nav  ${isVisible ? 'visible '  :'' }`}>   
-                    <button id="cerrar-menu" onClick={ClickClose}><i class="fa-solid fa-x"></i></button>     {/*  cerrar menu*/}
-                    <ul id="BurguerLinks" className="nav-list ">
-                       <li   className="list-group-item hoverActiveCursor BurguerLinks"><a href="#create-post"><i className="fas fa-house"></i> Inicio</a></li>
+               
+               {/* menu invisible */}
+                <nav id="navbarBurguer" className={`col-10 nav  ${isVisible ? 'visible '  :'' }`}>  
+                   <div>
+                     <button id="cerrar-menu" onClick={ClickClose}><i class="fa-solid fa-x"></i></button>
+                     <ul id="BurguerLinks" className="nav-list ">
+                        <a href="#" className=" logo">UNIVOZ</a> 
+                        <li   className="list-group-item hoverActiveCursor BurguerLinks"><a href="#create-post"><i className="fas fa-house"></i> Inicio</a></li>
                         <li   className="list-group-item hoverActiveCursor BurguerLinks "><a href="/normativas"><i className="fas fa-circle-info"></i> Información</a> </li>
-                        <li  className="list-group-item hoverActiveCursor BurguerLinks"><a href="/quienes-somos"><i className="fas fa-person"></i> Quiénes Somos</a></li>
-                        <li   className="list-group-item hoverActiveCursor BurguerLinks"><a href="/Login"><i class="fa-solid fa-right-to-bracket"></i>Iniciar sesión</a></li>
+                        <li className="list-group-item hoverActiveCursor BurguerLinks"><a href="/quienes-somos"><i className="fas fa-person"></i> Quiénes Somos</a></li>
+                        <li    className="list-group-item hoverActiveCursor BurguerLinks"><a href="/Login"><i class="fa-solid fa-right-to-bracket"></i>Iniciar sesión</a></li>
                     </ul>
+                    </div>
+                   
                 </nav> 
             </div>
         </header>
