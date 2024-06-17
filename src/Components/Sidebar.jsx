680b836b114c44edfcf9d28fb96c477e4338cae4
Fran-jeanne-sanchez <franciscajeannette0320@gmail.com>
2024-06-16 22:42:07 -0400

    cambios sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,22 +1,33 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './Estructura.css';
+
 const Sidebar = () => {
+    const location = useLocation();
 
     return (
-        <>
-
-            <nav className="sidebar">
-                <div className="menu">
-                    <ul>
-                        <li className="list-group-item active"><a href="/"><i className="fas fa-house"></i> Inicio</a></li>
-                        <li className="list-group-item "><a href="/normativas"><i className="fas fa-circle-info"></i> Información</a>
-                        </li>
-                        <li className="list-group-item"><a href="/quienes-somos"><i className="fas fa-person"></i> Quiénes Somos</a></li>
-                    </ul>
-                </div>
-            </nav>
-
-        </>
-    )
+        <nav className="sidebar">
+            <div className="menu">
+                <ul>
+                    <li
+                        className={`list-group-item ${location.pathname === '/' ? 'active' : ''}`}
+                    >
+                        <a href="/"><i className="fas fa-house"></i> Inicio</a>
+                    </li>
+                    <li
+                        className={`list-group-item ${location.pathname === '/normativas' ? 'active' : ''}`}
+                    >
+                        <a href="/normativas"><i className="fas fa-circle-info"></i> Información</a>
+                    </li>
+                    <li
+                        className={`list-group-item ${location.pathname === '/quienes-somos' ? 'active' : ''}`}
+                    >
+                        <a href="/quienes-somos"><i className="fas fa-person"></i> Quiénes Somos</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
-export default Sidebar
+export default Sidebar;
